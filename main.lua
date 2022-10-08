dba390975f0bba5757c3fc507bcb30574263e498
fantflasche <115209350+ztrirF080709@users.noreply.github.com>
2022-10-08 22:08:44 +0200

    zeit

--- main.lua
+++ main.lua
@@ -8,10 +8,11 @@ jumpKauntersave = 1
 start = 0
 resettime = 3
 skin = 0
+text = "0"
 fallspeed = 1
 y = 488
 function love.load()
-    froggy = love.graphics.newImage("froggy.png")
+    froggy = love.graphics.newImage("froggy.png") player = froggy
     floor = love.graphics.newImage("floor.png")
     spike = love.graphics.newImage("spike.png")
     piu = love.audio.newSource("piu.mp3","static")
@@ -30,6 +31,7 @@ function love.draw()
         love.graphics.draw(floor, x, 0)
         love.graphics.draw(floor, x+800, 0)
         love.graphics.draw(spike, xSpike+x, 500-2)
+        love.graphics.print( text, 750, 20)
     end
     if start == 0 then
         love.graphics.draw(startbaton, 350, 200)
@@ -37,12 +39,12 @@ function love.draw()
         --love.graphics.draw(, x+800, 0)
     end
     if skin == 1 then start = 3
-        love.graphics.draw(slime_and_umbrella, 350, 100)
-        love.graphics.draw(slime, 350, 150)
-        love.graphics.draw(plane, 350, 200)
-        love.graphics.draw(froggy2, 350, 250)
-        love.graphics.draw(froggyshild, 350, 300)
-        love.graphics.draw(froggy, 350, 350)
+        love.graphics.draw(slime_and_umbrella, 350, 100)--
+        love.graphics.draw(slime, 350, 150)--
+        love.graphics.draw(plane, 350, 200)--
+        love.graphics.draw(froggy2, 350, 250)--
+        love.graphics.draw(froggyshild, 350, 300)--
+        love.graphics.draw(froggy, 350, 350)--
         love.graphics.draw(anvil, 350, 400)
 
     end
@@ -50,18 +52,18 @@ end
 function love.update()
     x=x-speed
     start=start
-    seconds = love.timer.getTime()-resettime
+    seconds = love.timer.getTime()-resettime text = seconds
     if x<-800 then x=0 end
     speed = seconds
     if speed > 13 then speed=13 end
     y=y+fallspeed
     if y > 488 then y=488 end
     if y == 488 then jumpKaunter = jumpKauntersave end
-    if playerSkin == "froggy" then   jumpKauntersave = 1 fallspeed = 1 end
-    if playerSkin == "slime" then  jumpKauntersave = 2 fallspeed = 1 end
-    if playerSkin == "anvil" then  jumpKauntersave = 1  fallspeed = 9 end
-    if playerSkin == "slime-and-umbrella" then  jumpKauntersave = 2  fallspeed = 0.4
-    if playerSkin == "bird" then  jumpKauntersave = 10  fallspeed = 1 end end
+    if player == froggy then   jumpKauntersave = 1 fallspeed = 1 end
+    if player == slime then  jumpKauntersave = 2 fallspeed = 1 end
+    if player == anvil then  jumpKauntersave = 1  fallspeed = 9 end
+    if player == slime_and_umbrella then  jumpKauntersave = 2  fallspeed = 0.4 end
+    if player == plane then  jumpKauntersave = 10  fallspeed = 0.8 end 
     if xSpike + x >290 and xSpike + x <310 and y >480 and y <500 then start = 0 speed =0 end
 end
 function love.keypressed(key, scancode, isrepeat)
@@ -80,4 +82,13 @@ end
 function love.mousepressed(x,y, button, istouch)
    if y  >150 and y <250 and x  >300 and x <400 then start = 1 skin = 0 end
    if y  >250 and y <350 and x  >300 and x <400 then skin = 1  start = 3 end
+    if skin == 1 then  
+        if y  >50 and y <120 and x  >300 and x <400 then player = slime_and_umbrella end
+        if y  >100 and y <200 and x  >300 and x <400 then player = slime end
+        if y  >150 and y <225 and x  >300 and x <400 then player = plane end
+        if y  >275 and y <290 and x  >300 and x <400 then player = froggy2 end
+        if y  >295 and y <325 and x  >300 and x <400 then player = froggyshild end
+        if y  >330 and y <390 and x  >300 and x <400 then player = froggy end
+        if y  >395 and y <450 and x  >300 and x <400 then player = anvil end
+    end
 end
