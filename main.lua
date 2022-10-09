d530138fc29458c2b1e648a8218c6b9be4ee1f8b
fantflasche <115209350+ztrirF080709@users.noreply.github.com>
2022-10-09 11:19:13 +0200

    skin auswal

--- main.lua
+++ main.lua
@@ -34,7 +34,7 @@ function love.draw()
         love.graphics.draw(spike, xSpike+x, 500-2)
         love.graphics.print( text, 750, 20)
     end
-    if start == 0 then love.timer.sleep(s)
+    if start == 0 then 
         love.graphics.draw(startbaton, 350, 200)
         love.graphics.draw(skinbaton, 350, 300)
         love.graphics.print( points, 750, 20) 
@@ -82,8 +82,7 @@ function love.keypressed(key, scancode, isrepeat)
     if key == "s" then  start = 0  skin = 0 end
 end
 function love.mousepressed(x,y, button, istouch)
-   if y  >150 and y <250 and x  >300 and x <400 then start = 1 skin = 0 end
-   if y  >250 and y <350 and x  >300 and x <400 then skin = 1  start = 3 end
+   
     if skin == 1 then  
         if y  >50 and y <120 and x  >300 and x <400 then player = slime_and_umbrella end
         if y  >100 and y <200 and x  >300 and x <400 then player = slime end
@@ -92,5 +91,9 @@ function love.mousepressed(x,y, button, istouch)
         if y  >295 and y <325 and x  >300 and x <400 then player = froggyshild end
         if y  >330 and y <390 and x  >300 and x <400 then player = froggy end
         if y  >395 and y <450 and x  >300 and x <400 then player = anvil end
+    end
+    if start == 0 then
+        if y  >150 and y <250 and x  >300 and x <400 then start = 1 skin = 0 end
+        if y  >250 and y <350 and x  >300 and x <400 then skin = 1  start = 3 end
     end
 end
